ab9a7d953318edceb516e9d634c65e9e69f93473
pavan kalyan <mpavankalyan9491@gmail.com>
2025-05-09 16:43:03 +0530

    donee

---
+++ Desktop/monoproject/vishnu/commsec/core/src/main/java/com/cba/core/models/Hello.java
@@ -0,0 +1,36 @@
+package com.cba.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+   
+public class Hello {
+
+    @ValueMapValue
+    private String text;
+
+
+    @ValueMapValue
+    private String path;
+
+    @ValueMapValue
+    private String text2;
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+
+    public String getText2() {
+        return text2;
+    }
+}
